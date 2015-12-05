80f63d1309758849ba33551973c4f5802246c49f
Kris Burke <kris.burke611@gmail.com>
2015-12-04 19:49:19 -0500

    Completed _.map function

--- src/underbar.js
+++ src/underbar.js
@@ -105,6 +105,7 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    //might be better to write contains first
   };
 
 
@@ -113,6 +114,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(item){
+      results.push(iterator(item));
+    });
+    return results;
   };
 
   /*
@@ -121,7 +127,7 @@
    * as an example of this.
    */
 
-  // Takes an array of objects and returns and array of the values of
+  // Takes an array of objects and returns an array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
   _.pluck = function(collection, key) {
