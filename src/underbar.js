ece077741a2b554a27df13d68e5b28b8248d1ca9
Kris Burke <kris.burke611@gmail.com>
2015-12-08 20:26:31 -0500

    Whitespace changes

--- src/underbar.js
+++ src/underbar.js
@@ -198,7 +198,6 @@
     // TIP: There's a very clever way to re-use every() here.
   };
 
-
   /**
    * OBJECTS
    * =======
@@ -218,6 +217,7 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+
   };
 
   // Like extend, but doesn't ever overwrite a key that already
